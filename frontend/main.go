d9f5776ec0a7620c48235c57dc2c4909c69c5ca1
Urcra <urcra@urcra.com>
2018-04-07 14:03:29 +0200

    File uploads on frontend

--- frontend/main.go
+++ frontend/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "POST" {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		f, err := os.Create("./uploads/" + handler.Filename)
+
+		buf := bytes.NewBuffer(nil)
+		io.Copy(buf, file)
+		f.Write(buf.Bytes())
+		defer f.Close()
+		defer file.Close()
+
+		fmt.Fprintf(w, "%v", handler.Header)
+	}
+}
+
 func main() {
+	fmt.Println("Starting go web server on port 8080")
 	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.HandleFunc("/upload", upload)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
