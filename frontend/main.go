1dafa62cb9f970e9440fbc53f092e3aee7beb5b0
Urcra <urcra@urcra.com>
2018-04-07 15:31:58 +0200

    Saving binaries as empty files to wait for backend

--- frontend/main.go
+++ frontend/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"bytes"
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func upload(w http.ResponseWriter, r *http.Request) {
@@ -27,21 +28,39 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		defer f.Close()
 		defer file.Close()
 
-		h := sha1.New()
+		h := sha256.New()
 
 		h.Write(buf.Bytes())
 
 		bs := h.Sum(nil)
 
+		f1, _ := os.Create("./analysis/" + hex.EncodeToString(bs))
+		f2, _ := os.Create("./binaries/" + hex.EncodeToString(bs))
+
+		defer f1.Close()
+		defer f2.Close()
+
+		f2.Write(buf.Bytes())
+
 		http.Redirect(w, r, "/reports/"+hex.EncodeToString(bs), 301)
-		fmt.Fprintf(w, "%v", handler.Header)
+	}
+}
+
+func getReport(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		fmt.Println(r.RequestURI)
+		hash := strings.Split(r.RequestURI, "/")[2]
+
+		fmt.Println(hash)
 	}
 }
 
 func main() {
 	fmt.Println("Starting go web server on port 8080")
-	http.Handle("/", http.FileServer(http.Dir("./web")))
 	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/reports/", getReport)
+	http.Handle("/", http.FileServer(http.Dir("./web")))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
