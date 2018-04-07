d0eb29670d3640ad149ca661e8d0d1c3b1ad94ba
Mathias Hall-Andersen <mathias@hall-andersen.dk>
2018-04-07 14:09:18 +0200

    Added simple start script for RabbitMQ

---
+++ broker/run.sh
@@ -0,0 +1,10 @@
+docker pull rabbitmq:3
+docker run -d --hostname broker \
+    -e RABBITMQ_ERLANG_COOKIE=6fghihH162 \
+    -e RABBITMQ_DEFAULT_USER=broker \
+    -e RABBITMQ_DEFAULT_PASS=xl65x7jhacv \
+    -p 4369:4369 \
+    -p 5671:5671 \
+    -p 5672:5672 \
+    -p 25672:25672 \
+    --name rabbit rabbitmq
